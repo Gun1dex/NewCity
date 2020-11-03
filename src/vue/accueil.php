04f3cefde0326255c64e6d94881b5f92bf29ddee
Antoine Padé <antoine62217a@gmail.com>
2020-11-03 09:33:31 +0100

    Accueil

--- src/vue/accueil.php
+++ src/vue/accueil.php
@@ -4,32 +4,33 @@ ob_start();
 
 ?>
 
-    <div class="columns is-mobile is-centered">
-        <div class="column is-half" id="textCenter">
-            <article class="mainMenu">
+<div class="columns is-mobile is-centered">
+    <div class="column is-half" id="textCenter">
+        <article class="mainMenu">
+            <div class="content">
+                <p class="title"> NewCity</p>
                 <div class="content">
-                    <p class="title"> NewCity</p>
-                    <div class="content">
-                        <div class="columns is-mobile is-centered">
-                            <div class="column is-half" id="textCenter">
-                                <button><a href="index.php?page=jeu">Jouer</a></button>
-                            </div>
+                    <div class="columns is-mobile is-centered">
+                        <div class="column is-half" id="textCenter">
+                            <button><a href="index.php?page=jeu"><?php echo $play; ?></a></button>
                         </div>
-                        <div class="columns is-mobile is-centered">
-                            <div class="column is-half" id="textCenter">
-                                <button><a href="index.php?page=jeu"> Comment Jouer ?</a></button>
+                    </div>
+                    <div class="columns is-mobile is-centered">
+                        <div class="column is-half" id="textCenter">
+                            <button><a href="index.php?page=jeu"> <?php echo $howPlay; ?></a></button>
 
-                            </div>
                         </div>
                     </div>
                 </div>
-            </article>
-        </div>
-
+            </div>
+        </article>
     </div>
+
+</div>
 
 
 <?php
 $contenu = ob_get_clean();
 
 require_once("../src/vue/base.php");
+
